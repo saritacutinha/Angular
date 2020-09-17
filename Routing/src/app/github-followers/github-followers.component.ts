e49af2b59b87d3c2eae87666c1074af20961c978
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-09-17 21:06:35 +0930

    Adding Query parameters, Combining Observables

--- Routing/src/app/github-followers/github-followers.component.ts
+++ Routing/src/app/github-followers/github-followers.component.ts
@@ -1,18 +1,33 @@
-import { GithubFollowersService } from './../services/github-followers.service';
-import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { GithubFollowersService } from "./../services/github-followers.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import "rxjs/add/observable/combineLatest";
 
 @Component({
-  selector: 'github-followers',
-  templateUrl: './github-followers.component.html',
-  styleUrls: ['./github-followers.component.css']
+  selector: "github-followers",
+  templateUrl: "./github-followers.component.html",
+  styleUrls: ["./github-followers.component.css"],
 })
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private service: GithubFollowersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap,
+    ]).subscribe((params) => {
+      let id = params[0].get("id");
+      let page = params[1].get("page");
+
+      this.service
+        .getAll()
+        .subscribe((followers) => (this.followers = followers));
+    });
   }
 }
