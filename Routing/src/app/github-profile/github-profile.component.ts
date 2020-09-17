daf8e111fe6cc17ac537642b5b02ca924fae2c3e
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-09-17 20:00:27 +0930

    Retrieving the parameters methods

--- Routing/src/app/github-profile/github-profile.component.ts
+++ Routing/src/app/github-profile/github-profile.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-github-profile',
-  templateUrl: './github-profile.component.html',
-  styleUrls: ['./github-profile.component.css']
+  selector: "app-github-profile",
+  templateUrl: "./github-profile.component.html",
+  styleUrls: ["./github-profile.component.css"],
 })
 export class GithubProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-  }
+    //Following method used when component is not initialised multiple times
+    /*this.route.paramMap.subscribe((params) => {
+      let id = +params.get('id');
+      console.log(id); });*/
 
+    this.route.snapshot.paramMap.get("id");
+  }
 }
